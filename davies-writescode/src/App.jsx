30909e89410e8033ec43b3872caef7e182e89de7
Davies Gotosa <daviesgotosa10@gmail.com>
2024-12-14 12:00:04 +0200

    Created athe base files

---
+++ davies-writescode/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-black">Hello World!!!</h1>
+    </div>
+  );
+}
+
+export default App;
